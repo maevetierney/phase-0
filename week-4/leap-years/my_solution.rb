10a2deb926838d4b985add9091d8ebd3b6daf053
Maeve Tierney <maevet27@gmail.com>
2016-01-22 14:31:56 -0800

    cleaning

---
+++ week-4/leap-years/my_solution.rb
@@ -0,0 +1,21 @@
+# Leap Years
+
+# I worked on this challenge [by myselfs].
+
+
+# Your Solution Below
+
+def leap_year?(year)
+	if 
+		(year) / 4.to_i/ 100.to_i
+		return true
+	end 
+	if 
+		(year)/ 400.to_i
+		return true
+	end 
+	if 
+		(year)/ 4.to_i/ 400.to_i
+		return false
+	end 
+end 
