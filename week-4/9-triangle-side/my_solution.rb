669d4a7098316fb77b987e3d28eaaccce6762ae0
Maeve Tierney <maevet27@gmail.com>
2016-01-21 17:20:14 -0800

    triangle challenge

---
+++ week-4/9-triangle-side/my_solution.rb
@@ -0,0 +1,15 @@
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+
+def valid_triangle?(a, b, c)
+  if ( a <= 0 || b <= 0 || c <= 0 )
+    return false  
+  elsif   a + b > c && b + c > a && a + c > b 
+    return true
+  else 
+    return false
+  end
+end
