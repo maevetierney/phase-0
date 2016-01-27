024c32fe95c1e2ff3eb4e505d50bd183bffed20d
Maeve Tierney <maevet27@gmail.com>
2016-01-26 17:18:17 -0800

    redo of smallest integer

--- week-4/smallest-integer/my_solution.rb
+++ week-4/smallest-integer/my_solution.rb
@@ -16,6 +16,20 @@
 # Your Solution Below
 
 def smallest_integer(list_of_nums)
+	i = list_of_nums[0]
+	smallest_integer.each do |number|
+		i < 1
+	return number 
+	if 
+		list_of_nums[ ]
+		return nil
+		end 
+	end  
+end
+
+=begin
+#Code I turned in, original code(above is the new code i made using iteration)
+def smallest_integer(list_of_nums)
 	list_of_nums==[0]
 	if 
 		list_of_nums == []
@@ -28,3 +42,17 @@ def smallest_integer(list_of_nums)
 		return list_of_nums.min 
 	end 
 end 
+	
+=end
+
+# Smallest Integer
+
+# I worked on this challenge [by myself, with: ].
+
+# smallest_integer is a method that takes an array of integers as its input
+# and returns the smallest integer in the array
+#
+# +list_of_nums+ is an array of integers
+# smallest_integer(list_of_nums) should return the smallest integer in +list_of_nums+
+#
+# If +list_of_nums+ is empty the method should return nil
