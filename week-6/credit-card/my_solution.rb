b103619686413f68690f3d4560ad8485cb8f0eeb
Maeve Tierney <maevet27@gmail.com>
2016-02-07 21:06:05 -0800

    bingo

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -1,8 +1,8 @@
 # Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [ with: ].
-# I spent [#] hours on this challenge.
+# I worked on this challenge [ with: John Maguire ].
+# I spent [1.25] hours on this challenge.
 
 # Pseudocode
 =begin
@@ -24,9 +24,59 @@ DEFINE method, which takes an array as an argument
 # Don't forget to check on initialization for a card length
 # of exactly 16 digits
 
+# class CreditCard
+  
+#   attr_accessor :cc_num
+  
+#   def initialize(cc_num)
+#     @cc_num = cc_num
+    
+#     if cc_num.to_s.length != 16 
+#       raise ArgumentError.new("Incorrect number length")
+#     end 
+    
+#   end
+
+#   def check_card(number)
+#     @cc_num = number
+#     dub_array = []
+#     string_arr = number.to_s.each_char.each_slice(1).map{|x| x.join}
+#     int_arr = string_arr.map{|i| i.to_i}
+    
+#     int_arr.each_index{|item|
+#       if item == 0 || item%2 == 0
+#         d_num = int_arr[item]*2
+#         int_arr[item] = d_num
+#         end
+#       }
+#    int_arr.each_index{|item|
+#      if int_arr[item].to_s.length == 2
+#         str_d_num = int_arr[item].to_s.each_char.each_slice(1).map{|x| x.join}
+#         
+#      end
+#      }
+    
+#     if int_arr.reduce(:+)%10 == 0
+#       return true
+#     else 
+#       return false
+#     end
+    
+#   end
+  
+       
+# end
+
+example = CreditCard.new(4563960122001999)
+p example.check_card(4563960122001999)
+
+  
+
+# Refactored Solution
 class CreditCard
   
   attr_accessor :cc_num
+  
   def initialize(cc_num)
     @cc_num = cc_num
     
@@ -35,13 +85,12 @@ class CreditCard
     end 
     
   end
+
   def check_card(number)
     @cc_num = number
     dub_array = []
     string_arr = number.to_s.each_char.each_slice(1).map{|x| x.join}
     int_arr = string_arr.map{|i| i.to_i}
-    
-    
     
     int_arr.each_index{|item|
       if item == 0 || item%2 == 0
@@ -68,38 +117,18 @@ class CreditCard
   
        
 end
+# Reflection-----
+# What was the most difficult part of this challenge 
+# for you and your pair?
+# => I would say the concept of putting this into an array,
+# => putting, it into a string, and then iterating 
+# => and putting it all back together like that. 
 
-example = CreditCard.new(4563960122001999)
-p example.check_card(4563960122001999)
+# What new methods did you find to help you when you 
+# refactored?
+# => When I refactored we found the reduce method 
+# => which allowed us to see if the total number added
+# => of all of the digits is a multiple of ten. This helped
+# => us easily do the last part of the problem. 
 
-  
-#  if d_num.length == 2
-#           str_d_num = d_num.to_s.each_char.each_slice(1).map{|x| x.join}
-#           int_d_num = str_d_num.map{|i| i.to_i}
-#           int_arr.push(d_num_arr[0])
-#         }      
-      
-# [1,2,3,4,5]
-# [1,0]
-# [1,2,3,4,[1,0]]      
-# # array.each |item|
-# #   d_item = item*2
-# #     if d_item > 9
-# #       d_item.digits.each{}
-
-# str_array = 876885.to_s.each_char.each_slice(1).map{|x| x.join}
-# p int_array = str_array.map{|i| i.to_i}
-
-# number = 10567889
-# p number.to_s.
-
-# Refactored Solution
-
-
-
-
-
-
-
-
-# Reflection
+# What concepts or learnings were you able to solidify in this challenge?
